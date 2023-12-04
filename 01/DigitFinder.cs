c20f601b399c4524f320dcfc832c45f0513cb964
Turwaith <nicolasbaur@turwaith.dev>
2023-12-04 20:51:14 +0100

    Adds first day of challenge

---
+++ 01/DigitFinder.cs
@@ -0,0 +1,52 @@
+namespace _01;
+
+public class DigitFinder
+{
+    public int FindRelevantSum()
+    {
+        List<int> allValues = new();
+        int sum = 0;
+
+        foreach (string line in LoadFileContent())
+        {
+            (char?, char?) digits = FindFirstAndLastDigit(line);
+            string combined = digits.Item1.ToString() + digits.Item2.ToString();
+            allValues.Add(int.Parse(combined));
+        }
+
+        foreach (int value in allValues)
+        {
+            sum += value;
+        }
+
+        return sum;
+    }
+
+    private string[] LoadFileContent()
+    {
+        string[] lines = File.ReadAllLines(@"Resources\resourceFile.txt");
+        
+        return lines;
+    }
+
+    private (char?, char?) FindFirstAndLastDigit(string input)
+    {
+        char? firstDigit = null;
+        char? lastDigit = null;
+
+        foreach (char c in input)
+        {
+            if (char.IsDigit(c))
+            {
+                if (firstDigit == null)
+                {
+                    firstDigit = c;
+                }
+
+                lastDigit = c;
+            }
+        }
+
+        return (firstDigit, lastDigit);
+    }
+}
---
+++ 01/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DigitFinder digitFinder = new();
+
+            int result = digitFinder.FindRelevantSum();
+            
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
